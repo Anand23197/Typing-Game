5ee605a4f582e42dcc692b5e721988832ed26438
ANAND VISHWAKARMA <anand23197@gmail.com>
2022-06-24 18:05:47 +0530

    second

--- index.js
+++ index.js
@@ -1,38 +1,60 @@
-const arr = ['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J','K','L',
-              'M', 'N', 'O', 'P', 'Q', 'R','S','T','U','V','W','X',
-              'Y','Z'];
-
+const str = 'ABCDEFGHIJKLMNOPQRST';
+var charlen = str.length;
 const character = document.querySelector('#character');
 const inputText = document.querySelector('input')
 const ms = document.querySelector('#ms');
 const sec = document.querySelector('#second');
+const myBestTime = document.querySelector('#my-best-time');
+let count = 0;
 // taking input in text and changing character
-inputText.addEventListener('keyup', (e)=>{
-           let upChar =  e.key.toUpperCase().charCodeAt(0);
-           console.log(upChar.charCodeAt(0));
-  //  console.log(e.target.value.toUpperCase() + " " + e.key);
-    let innerChar = arr[Math.floor(Math.random()*arr.length)];
-    console.log(innerChar.charCodeAt(0));
-    if(innerChar.charCodeAt(0) === upChar){
-        console.log("good");
+function generateRandomCharacter(){
+    let innerChar = str.charAt(Math.floor(Math.random()*charlen));
+    return innerChar;
+}
+character.innerHTML = generateRandomCharacter();
+
+let start = 0;
+inputText.addEventListener('keydown', (e)=>{
+    //start the game
+    if(start <= 0){
+        startGame();
+        start++;
     }
-    character.innerHTML = innerChar;
     
-//start the game
-//startGame();
+
+    let keyText = e.key.toUpperCase();
+    let divChar = character.innerHTML.charAt(0);
+    if(divChar === keyText ){
+        character.innerHTML = generateRandomCharacter();
+        count++;
+    }else{
+       
+    }
+
+    //stop game and store best score
+ if(count >= 5){
+    clearInterval(interval);
+    const oldScore = Number(localStorage.getItem('timing'));
+    if(oldScore == 0 || oldScore > Number(ms.innerHTML)){   
+         localStorage.setItem('timing' , ms.innerHTML );
+         character.innerHTML = "Success!";
+    }else{
+        character.innerHTML = "Failure!";
+    }
+ }
 })
 
 
-let startTime = Date.now();
-let elapsedTime;
-// function startGame(){
-// let interval = setInterval(()=>{
-//     elapsedTime = Date.now() - startTime;
-//     let tim = (elapsedTime/1000).toFixed(3);
-//     ms.innerHTML = tim;
-// }, 100);
-// }
-
+const startTime = Date.now();
+ var elapsedTime;
+var interval;
+function startGame(){
+ interval = setInterval(()=>{
+    elapsedTime = Date.now() - startTime;
+    let tim = (elapsedTime/1000).toFixed(3);
+    ms.innerHTML = tim;
+}, 100);
+}
 
 
 
